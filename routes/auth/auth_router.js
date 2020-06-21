14bacbef5d123b403b54667f54b9a3bd44d08583
karapeoples <kararpeoples@yahoo.com>
2020-06-21 04:19:50 -0500

    More Code

--- routes/auth/auth_router.js
+++ routes/auth/auth_router.js
@@ -17,53 +17,54 @@ const generateToken = (user) => {
 	return jwt.sign(payload, jwtSecret, options)
 }
 
-router.post('/register', (req, res) => {
-	const newUser = req.body
-	
-	const rounds = process.env.BCRYPT_ROUNDS || 8
-	const hash = bcryptjs.hashSync(newUser.password, rounds)
-	hash(newUser.password)
-	
-		const token = generateToken(newUser)
 
-	if (newUser) {
-		regUser
-			.add(newUser)
-			.then((user) => {
-				res.status(201).json({ user: newUser, token: token })
-			})
-			.catch((error) => {
-				res.status(418).json({ error: 'I am a Teapot!' })
-			})
-	} else {
-		res.status(400).json({
-			message: 'please provide all required information',
-		})
-	}
-})
+	router.post("/register", async (req, res) => {
+  let { email, password, firstName, lastName, role } = req.body;
+  const hash = bcrypt.hashSync(password, 8);
+  let userObj = {
+    email: email,
+    password: hash,
+    first_name: firstName,
+    last_name: lastName,
+    role: role
+  };
 
-router.post('/login', (req, res) => {
-	const logUser = req.body
-	
-	if (logUser) {
-		regUser
-			.findBy({ email: logUser.email })
-			.then(([user]) => {
-				// compare the password the hash stored in the database
-				if (user && bcryptjs.compareSync(password, user.password)) {
-					const token = generateToken(user)
-					res.status(200).json({  user:logUser, token:token })
-				} else {
-					res.status(401).json({ message: 'Invalid credentials' })
-				}
-			})
-			.catch((error) => {
-				res.status(418).json({ error: 'I am a Teapot' })
-			})
+  try {
+    // add new user to the db
+    let newUser = await Users.addUser(userObj);
+    let newUserId = newUser.id;
+
+    const token = genToken(newUser);
+    res
+      .status(201)
+      .json({ user: newUser, user_id: newUserId, token: token });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ errorMsg: error, message: "Was not able to register user" });
+  }
+});
+
+router.post('/login', async (req, res, next) => {
+	if (!req.body || !req.body.password || !req.body.email) {
+		next('A valid email and password are required.')
 	} else {
-		res.status(400).json({
-			message: 'please provide email and password',
-		})
+		let { email, password } = req.body
+
+		try {
+			// find user by email
+			const user = await Users.findBy({ email })
+
+			if (user && bcrypt.compareSync(password, user.password)) {
+				const roleInfo = await Users.findTypeById(user.id)
+				const token = genToken(user)
+				res.status(200).json({ user: user, user_id: roleInfo, token: token })
+			} else {
+				res.status(401).json({ message: 'Invalid Login Credentials' })
+			}
+		} catch (error) {
+			res.status(500).json({ errorMsg: error, message: 'Was not able to login user' })
+		}
 	}
 })
 
